9e8da41c75164167f20a6bd76abaf65768150c16
Aayush-Gupta04 <118243229+Aayush-Gupta04@users.noreply.github.com>
2024-04-12 01:38:24 +0530

    Add files via upload

---
+++ real_estate_management/real_estate_management/Real_Estate_Management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package real_estate_management;
+
+/**
+ *
+ * @author Mudit
+ */
+public class Real_Estate_Management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
